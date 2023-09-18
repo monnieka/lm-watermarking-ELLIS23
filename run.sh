7fd5f90542f1a37b7f0a1347e8159dccc0102b4b
Monica Millunzi <82164235+monnieka@users.noreply.github.com>
2023-09-18 18:15:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH -N _ # 2 - number of nodes to allocate
-#SBATCH --ntasks-per-node=_ # 16 -number of processes per compute node
-#SBATCH --cpus-per-task=_ # 2 number of cores allocated per process
-#SBATCH --gres=gpu:_ # number of gpus per node
+#SBATCH -N 1 # 2 - number of nodes to allocate
+#SBATCH --ntasks-per-node=8 # 16 -number of processes per compute node
+#SBATCH --cpus-per-task=1 # 2 number of cores allocated per process
+#SBATCH --gres=gpu:1 # number of gpus per node
 #SBATCH -t 1:00:00 # total run time of the job allocation
-#SBATCH --mem=_GB # memory request per node
-#SBATCH -o job.out # for stdout redirection
-#SBATCH -e job.err # for stderr redirection
+#SBATCH --mem=52GB # memory request per node
+#SBATCH -o out/out_%A_%J.out
+job.out # for stdout redirection
+#SBATCH -e err/out_%A_%J.err # for stderr redirection
 #SBATCH -p boost_usr_prod # partition for resource allocation
 #SBATCH -A tra23_ELLIS # account name 
 #SBATCH --reservation s_tra_Ellis1809 # after monday change to s_tra_Ellis
